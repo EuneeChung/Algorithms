6df81c334b636ecb7481bce82faa475782170534
EuneeChung <chungee375@gmail.com>
2020-04-09 18:24:51 +0900

    [#11022] A+B -8

---
+++ src/11022.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main()= with(Scanner(System.`in`)){
+    val x=nextInt()
+    for(i in 1..x){
+        val a=nextInt()
+        val b=nextInt()
+        println("Case #$i: $a + $b = ${a+b}")
+    }
+}
