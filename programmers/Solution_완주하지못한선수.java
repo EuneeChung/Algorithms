e45fd41dd2e9b582ee108cadac63fbf1fed08130
EuneeChung <chungee375@gmail.com>
2021-10-21 22:51:28 +0900

    [Level1] 완주하지 못한 선수 - IDE 사용 금지 - import 꼭 하기!!

---
+++ programmers/Solution_완주하지못한선수.java
@@ -0,0 +1,22 @@
+package programmers;
+
+import java.util.*;
+
+public class Solution_완주하지못한선수 {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap();
+
+        for (int i = 0; i < participant.length; i++) {
+            map.putIfAbsent(participant[i], 0);
+            map.put(participant[i], map.get(participant[i]) + 1);
+        }
+
+        for (int i = 0; i < completion.length; i++) {
+            map.put(completion[i], map.get(completion[i]) - 1);
+            if (map.get(completion[i]) == 0) map.remove(completion[i]);
+        }
+        Iterator key = map.keySet().iterator();
+
+        return key.next().toString();
+    }
+}
