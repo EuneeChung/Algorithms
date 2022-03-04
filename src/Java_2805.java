9f1c28f18ee62ba8592cc3024038fd8ea655c794
EuneeChung <chungee375@gmail.com>
2022-03-04 17:22:04 +0900

    [BOJ JAVA] 2805 나무 자르기 - 이분 탐색 - 파라메트릭 서치

--- src/Java_2805.java
+++ src/Java_2805.java
@@ -3,6 +3,43 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Java_2805 {
+    static int M;
+    static int[] trees;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        trees = new int[N];
+        st = new StringTokenizer(br.readLine());
+        long left=0, right=0,midH=0;
+        for (int i = 0; i <N; i++) {
+            trees[i]=Integer.parseInt(st.nextToken());
+            right = Math.max(right, trees[i]);
+        }
+
+        while(left+1<right){
+            midH = (left+right)/2;
+            if(check(midH)) left =midH;
+            else right=midH;
+        }
+        System.out.println(left);
+
+    }
+    // 결정문제
+    // height 를 기준으로 잘랐을 경우 M 길이 이상의 나무를 얻을 수 있는지?
+    // height 길이가 짧을 수록 더 많은 길이의 나무를 얻을 수 있음
+    // 범위 left TTTTTTT FFFFFFFF right
+    public static boolean check(long height){
+        long length = 0;
+        for (int t : trees) {
+            if(t>height) length+= t-height;
+        }
+        return length>=M;
+    }
+
+    /* Solution 1
+    *
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -36,4 +73,5 @@ public class Java_2805 {
         }
         return length;
     }
+    */
 }
