0ed31ebfb4bb39cfc27e6629ae1a3a7fa740c9d2
EuneeChung <chungee375@gmail.com>
2022-03-17 00:46:01 +0900

    [BOJ JAVA] 7662 이중 우선순위 큐

--- src/Java_7662.java
+++ src/Java_7662.java
@@ -1,59 +1,67 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Java_7662 {
-    public static void main(String[] args)  throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         PriorityQueue<Integer> minPQ = new PriorityQueue<>();
         PriorityQueue<Integer> maxPQ = new PriorityQueue<>(Collections.reverseOrder());
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         int T = Integer.parseInt(br.readLine());
         int N;
         StringTokenizer st;
 
-        for(int t=0;t<T;t++){
+        for (int t = 0; t < T; t++) {
             minPQ.clear();
             maxPQ.clear();
+            map.clear();
             N = Integer.parseInt(br.readLine());
 
-            for(int n=0; n<N;n++){
+            for (int n = 0; n < N; n++) {
                 st = new StringTokenizer(br.readLine());
                 String operation = st.nextToken();
                 int num = Integer.parseInt(st.nextToken());
-                if(operation.equals("I")){
+                if (operation.equals("I")) {
                     maxPQ.add(num);
                     minPQ.add(num);
+                    int cnt = map.getOrDefault(num, 0);
+                    map.put(num, cnt + 1);
                 }
-                if(operation.equals("D")&& !minPQ.isEmpty()){
-
-                    if(num==-1){
-                        Integer min = minPQ.poll();
-                        maxPQ.remove(min);
-
-//                        while(minPQ.size()> maxPQ.size()){
-//                            maxPQ.add(min);
-//                        }
-                    }
-                    else{
-                        Integer max = maxPQ.poll();
-                        minPQ.remove(max);
-
-//                        while(minPQ.size()< maxPQ.size()){
-//                            minPQ.add(max);
-//                        }
-                    }
-
-
+                if (operation.equals("D") && map.size() != 0) {
+                    if (num == -1) delete(minPQ, map);
+                    else delete(maxPQ, map);
                 }
             }
-            if(minPQ.isEmpty()) sb.append("EMPTY");
-            else sb.append(maxPQ.poll()).append(" ").append(minPQ.poll());
-            if(t!=T-1) sb.append("\n");
+
+            if (map.size() == 0) sb.append("EMPTY");
+            else {
+                int maxOrMin = delete(maxPQ, map);
+                sb.append(maxOrMin).append(" ");
+                if (map.size() > 0) maxOrMin = delete(minPQ, map);
+                sb.append(maxOrMin);
+            }
+            if (t != T - 1) sb.append("\n");
         }
         System.out.print(sb);
+    }
+
+    static int delete(PriorityQueue<Integer> que, HashMap<Integer, Integer> map) {
+        int endNum = 0;
+        while (true) {
+            endNum = que.poll();
+            int cnt = map.getOrDefault(endNum, 0);
+            if (cnt == 0) continue;
+
+            if (cnt == 1) map.remove(endNum);
+            else map.put(endNum, cnt - 1);
+            break;
+        }
+        return endNum;
     }
 }
